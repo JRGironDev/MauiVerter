2521764fbefefaa2378594d29bdc71ab5feec1f5
José Quiñónez <joserobertoq@outlook.com>
2024-05-16 05:21:40 -0600

    Add ViewModels Folder

---
+++ MauiVerter/MVVM/Views/MenuView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiVerter.MVVM.ViewModels;
+
+namespace MauiVerter.MVVM.Views;
+
+public partial class MenuView : ContentPage
+{
+
+	public MenuView()
+	{
+		InitializeComponent();
+
+		BindingContext = new MenuViewModel();
+	}
+}
+
