32534c20b2fb44e577dbf650f3a14e50c0d0ba14
José Quiñónez <joserobertoq@outlook.com>
2024-05-18 04:38:35 -0600

    Add Converter View

---
+++ MauiVerter/MVVM/Views/ConverterView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiVerter.MVVM.ViewModels;
+
+namespace MauiVerter.MVVM.Views;
+
+public partial class ConverterView : ContentPage
+{
+
+	public ConverterView()
+	{
+		InitializeComponent();
+
+		BindingContext = new ConverterViewModel();
+	}
+}
+
--- MauiVerter/obj/Debug/net8.0-android/MauiVerter.AssemblyInfo.cs
+++ MauiVerter/obj/Debug/net8.0-android/MauiVerter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiVerter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+903b9229976e022b2c78496f70be2689476b622c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+253efd4a85ac157b5e07c460b61bcdc74b6b119a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiVerter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiVerter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
